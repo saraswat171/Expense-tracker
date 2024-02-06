296149669cecfeb664236140e375edb9b9f92c73
saraswat171 <saraswatchetan2020@gmail.com>
2024-02-06 18:50:13 +0530

    first commit

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Name is required"]
+    },
+    email: {
+        unique: true,
+        type: String,
+        require: [true, "Name is required"]
+    },
+
+    Income: {
+        unique: true,
+        type: Number,
+        require: [true, "Name is required"]
+    },
+    password: {
+        type: String,
+        require: [true, "Name is required"]
+    },
+    createAt: {
+        type:Date,
+        default:Date.now,
+    },
+
+})
+
+module.exports = mongoose.model("users", UsersSchema)
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   hghhjjkhjdfyjuty
+    </div>
+  );
+}
+
+export default App;
